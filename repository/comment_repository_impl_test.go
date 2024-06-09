91a34cab5ca9db551fcbf994cf94b9ee32d6a62c
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-09 21:09:44 +0700

    repository pattern

---
+++ repository/comment_repository_impl_test.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	belajar_golang_database_mysql "belajar-golang-database-mysql"
+	"belajar_golang_database_mysql/entity"
+	"context"
+	"fmt"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestCommentInsert(t *testing.T) {
+	commentRepository := NewCommentRepository(belajar_golang_database_mysql.GetConnection())
+
+	ctx := context.Background()
+	comment := entity.Comment{
+		Email:   "repository@test.com",
+		Comment: "Test Repository",
+	}
+
+	result, err := commentRepository.Insert(ctx, comment)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
+}
+
+func TestFindById(t *testing.T) {
+	commentRepository := NewCommentRepository(belajar_golang_database_mysql.GetConnection())
+
+	comment, err := commentRepository.FindById(context.Background(), 22)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(comment)
+}
+
+func TestFindAll(t *testing.T) {
+	commentRepository := NewCommentRepository(belajar_golang_database_mysql.GetConnection())
+
+	comments, err := commentRepository.FindAll(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for _, comment := range comments {
+		fmt.Println(comment)
+	}
+}
