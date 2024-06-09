a09044da5d609ac9cb26d5adc667adaee5dcba11
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-09 16:09:32 +0700

    tipe data column

--- database.go
+++ database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_database_mysql")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_database_mysql?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
