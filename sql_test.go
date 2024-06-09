9964988b9cc482703a74f9e1af26eb953285f97e
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-06-09 19:24:36 +0700

    auto increment

--- sql_test.go
+++ sql_test.go
@@ -165,3 +165,25 @@ func TestExecSqlParameter(t *testing.T) {
 
 	fmt.Println("Success insert new user")
 }
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	email := "bayu@gmail.com"
+	comment := "Test komen ke dua"
+
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	result, err := db.ExecContext(ctx, script, email, comment)
+	if err != nil {
+		panic(err)
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new comment with id", insertId)
+}
